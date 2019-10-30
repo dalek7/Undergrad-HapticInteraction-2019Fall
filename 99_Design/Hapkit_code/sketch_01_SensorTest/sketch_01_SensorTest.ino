2447654417d0046ecca45af07f79a2d1edfd424e
Seung@gram-win <ncraig@naver.com>
2019-10-30 12:12:14 +0900

    code for basic test

---
+++ 99_Design/Hapkit_code/sketch_01_SensorTest/sketch_01_SensorTest.ino
@@ -0,0 +1,23 @@
+
+int sensorPosPin = A2; // input pin for MR sensor
+int rawPos;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(sensorPosPin, INPUT); // set MR sensor pin to be an input
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  calculatePosition();
+  Serial.println(rawPos);
+}
+
+
+void calculatePosition()
+{
+  rawPos = analogRead(sensorPosPin);
+  
+}
